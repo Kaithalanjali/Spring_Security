29e653002164abccb511c14ce0121e05e60268f9
Srinivas Nagaram <snagaram@medline.com>
2023-09-25 15:13:29 +0530

    Made session stateless and disabled csrf, by changing springbootwebsecurityconfiguration

---
+++ src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.in28minutes.learnspringsecurity.basic;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class BasicAuthSecurityConfiguration {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        http.csrf((csrf) -> csrf.disable());
+        return http.build();
+    }
+}
